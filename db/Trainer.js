351e1d3c64cf77ed827b8bbd6f5480da89e41459
Isaias Pomales <impomales@gmail.com>
2018-05-02 17:04:31 -0400

    trainer model defined

--- db/Trainer.js
+++ db/Trainer.js
@@ -2,7 +2,23 @@ const Sequelize = require('sequelize');
 const db = require('./_db');
 
 const Trainer = db.define('trainer', {
-
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  class: {
+    type: Sequelize.STRING
+  },
+  badges: {
+    type: Sequelize.ARRAY(Sequelize.STRING)
+  },
+  items: {
+    type: Sequelize.ARRAY(Sequelize.STRING)
+  }
 });
 
 module.exports = Trainer;
